1a49af51563f94670ad5b2ba2cc83764320f2a40
Alek <aleksandrvolobuev7676@gmail.com>
2026-05-30 18:07:55 +0300

    fix github pages base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Site_GTA/', // 👈 ВАЖНО: имя твоего репозитория
   plugins: [react(), tailwindcss()],
 })
